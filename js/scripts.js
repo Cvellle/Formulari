2109ba022a2bbd5c1531dc22955efde26c07d938
ncvetic <cuekisa@yahoo.com>
2020-03-27 14:11:28 +0100

    a

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
 // localStorage.clear()
 // localStorage.removeItem("myListItems3");
 
-(function () {
-    function all() {
+function all() {
 
-        // PRINT WINDOW
-        const printButton = document.querySelector('#printButton');
-        printButton.onclick = function (e) {
-            window.print();
-        }
+    // PRINT WINDOW
+    const printButton = document.querySelector('#printButton');
+    printButton.onclick = function (e) {
+        window.print();
+    }
 
-        // APPEND
-        const appendInupt = function (e) {
-            const node = document.createElement("DIV");
-            const inp = document.createElement("TEXTAREA");
-            const changeFont = document.createElement("INPUT");
-            const areaName = document.createElement("INPUT");
+    // APPEND
+    const appendInupt = function (e) {
+        const node = document.createElement("DIV");
+        const inp = document.createElement("TEXTAREA");
+        const changeFont = document.createElement("INPUT");
+        const areaName = document.createElement("INPUT");
 
-            node.appendChild(inp);
-            node.insertBefore(changeFont, node.firstChild);
-            node.appendChild(areaName);
+        node.appendChild(inp);
+        node.insertBefore(changeFont, node.firstChild);
+        node.appendChild(areaName);
 
-            e.target.nextElementSibling.innerHTML += `
+        e.target.nextElementSibling.innerHTML += `
                 <div class="draggableDiv" >
                     <input type="range"/>
                     <textarea class="newInput" readonly="true"></textarea>
@@ -32,76 +31,48 @@
                 </div>
             `;
 
-            const newInputsList = document.querySelector(".myList");
-            localStorage.setItem('myListItems3', document.body.innerHTML);
+        const newInputsList = document.querySelector(".myList");
+        localStorage.setItem('myListItems3', document.body.innerHTML);
 
 
-            const newInp = document.querySelectorAll('.myList input[type="range"]');
-            newInp.forEach(inp => inp.addEventListener("mousemove", function () {
-                this.nextElementSibling.style.fontSize = this.value * 0.3 + "px";
-            }));
+        const newInp = document.querySelectorAll('.myList input[type="range"]');
+        newInp.forEach(inp => inp.addEventListener("mousemove", function () {
+            this.nextElementSibling.style.fontSize = this.value * 0.3 + "px";
+        }));
 
-            $(function () {
-                $(".newInput").parent().draggable();
-            });
+        $(function () {
+            $(".newInput").parent().draggable();
+        });
 
 
-            //  INPUT VALUES
-            "keyup change".split(" ").forEach(evt => {
-                const newInputs = document.querySelectorAll(".newInput + input");
-                const nameInputs = document.querySelectorAll(".areaName + input");
+        //  INPUT VALUES
+        "keyup change".split(" ").forEach(evt => {
+            const newInputs = document.querySelectorAll(".newInput + input");
+            const nameInputs = document.querySelectorAll(".areaName + input");
 
-                nameInputs.forEach(
-                    inputElement => inputElement.addEventListener(evt, function (e) {
-                        this.previousElementSibling.innerHTML = this.value;
-                        this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
-                        //AUTO SET DATE
-                        const date = new Date();
-                        if (this.value == "date") {
-                            this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
-                        }
-                    }, false)
-                )
-
-                newInputs.forEach(
-                    inputElement => inputElement.addEventListener(evt, function (e) {
-                        this.previousElementSibling.innerHTML = this.value;
-                        var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
-                        sameElements.forEach(textElement => {
-                            textElement.innerHTML = e.target.value;
-                        });
-                    }, false)
-                )
-
-            });
-
-            const divs = document.querySelectorAll(".draggableDiv");
-
-            divs.forEach(
-                inputElement => inputElement.addEventListener("mouseup", function (e) {
-                    localStorage.setItem('myListItems3', document.body.innerHTML);
+            nameInputs.forEach(
+                inputElement => inputElement.addEventListener(evt, function (e) {
+                    this.previousElementSibling.innerHTML = this.value;
+                    this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
+                    //AUTO SET DATE
+                    const date = new Date();
+                    if (this.value == "date") {
+                        this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
+                    }
                 }, false)
             )
 
-            divs.forEach(element => {
-                element.addEventListener("mouseover", allowDel)
-            });
+            newInputs.forEach(
+                inputElement => inputElement.addEventListener(evt, function (e) {
+                    this.previousElementSibling.innerHTML = this.value;
+                    var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
+                    sameElements.forEach(textElement => {
+                        textElement.innerHTML = e.target.value;
+                    });
+                }, false)
+            )
 
-            const deleteDiv1 = document.querySelectorAll('.myList button');
-
-            function allowDel() {
-                deleteDiv1.forEach(
-                    el => el.addEventListener("click", function (e) {
-                        this.parentElement.style.display = "none";
-                    }, false)
-                )
-            }
-
-        }
-
-        const appendInupts = document.querySelectorAll('.addInputs');
-        appendInupts.forEach(el => el.addEventListener("click", appendInupt)) = appendInupt;
-
+        });
 
         const divs = document.querySelectorAll(".draggableDiv");
 
@@ -115,81 +86,108 @@
             element.addEventListener("mouseover", allowDel)
         });
 
-        const deleteDiv = document.querySelectorAll('.myList button');
+        const deleteDiv1 = document.querySelectorAll('.myList button');
 
         function allowDel() {
-            deleteDiv.forEach(
+            deleteDiv1.forEach(
                 el => el.addEventListener("click", function (e) {
                     this.parentElement.style.display = "none";
                 }, false)
             )
         }
 
-}
+    }
 
-    const drD = document.querySelectorAll(".myList>div");
-    drD.forEach(
-        inputElement => inputElement.addEventListener("load", function (e) {
-            alert("a");
+    const appendInupts = document.querySelectorAll('.addInputs');
+    appendInupts.forEach(el => el.addEventListener("click", appendInupt)) = appendInupt;
+
+
+    const divs = document.querySelectorAll(".draggableDiv");
+
+    divs.forEach(
+        inputElement => inputElement.addEventListener("mouseup", function (e) {
+            localStorage.setItem('myListItems3', document.body.innerHTML);
         }, false)
     )
 
-    // OPEN FILE
-    const acceptInput = document.querySelectorAll('.fileInp');
-    acceptInput.forEach(element => {
-        element.addEventListener("change", loadFile)
+    divs.forEach(element => {
+        element.addEventListener("mouseover", allowDel)
     });
 
-    function loadFile(event) {
-        var output = event.target.parentNode;
-        output.style.backgroundImage = "url(" + URL.createObjectURL(event.target.files[0]) + ")";
-    };
+    const deleteDiv = document.querySelectorAll('.myList button');
 
-    // LOCAL STORAGE
-    var saved = localStorage.getItem('myListItems3');
-
-    if (saved) {
-        document.body.innerHTML = saved;
+    function allowDel() {
+        deleteDiv.forEach(
+            el => el.addEventListener("click", function (e) {
+                this.parentElement.style.display = "none";
+            }, false)
+        )
     }
 
-    //  INPUT VALUES
-    "keyup change".split(" ").forEach(evt => {
-        const newInputs = document.querySelectorAll(".newInput + input");
-        const nameInputs = document.querySelectorAll(".areaName + input");
+}
 
-        nameInputs.forEach(
-            inputElement => inputElement.addEventListener(evt, function (e) {
-                this.previousElementSibling.innerHTML = this.value;
-                this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
-                //AUTO SET DATE
-                const date = new Date();
-                if (this.value == "date") {
-                    this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
-                }
-            }, false)
-        )
+const drD = document.querySelectorAll(".myList>div");
+drD.forEach(
+    inputElement => inputElement.addEventListener("load", function (e) {
+        alert("a");
+    }, false)
+)
 
-        newInputs.forEach(
-            inputElement => inputElement.addEventListener(evt, function (e) {
-                this.previousElementSibling.innerHTML = this.value;
-                var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
-                sameElements.forEach(textElement => {
-                    textElement.innerHTML = e.target.value;
-                });
-            }, false)
-        )
+// OPEN FILE
+const acceptInput = document.querySelectorAll('.fileInp');
+acceptInput.forEach(element => {
+    element.addEventListener("change", loadFile)
+});
 
-    });
+function loadFile(event) {
+    var output = event.target.parentNode;
+    output.style.backgroundImage = "url(" + URL.createObjectURL(event.target.files[0]) + ")";
+};
 
-    //DRAG ONLOAD
-    $(function () {
-        $(".newInput").parent().draggable();
-    });
+// LOCAL STORAGE
+var saved = localStorage.getItem('myListItems3');
 
-    window.addEventListener("load", all);
+if (saved) {
+    document.body.innerHTML = saved;
+}
+
+//  INPUT VALUES
+"keyup change".split(" ").forEach(evt => {
+    const newInputs = document.querySelectorAll(".newInput + input");
+    const nameInputs = document.querySelectorAll(".areaName + input");
+
+    nameInputs.forEach(
+        inputElement => inputElement.addEventListener(evt, function (e) {
+            this.previousElementSibling.innerHTML = this.value;
+            this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
+            //AUTO SET DATE
+            const date = new Date();
+            if (this.value == "date") {
+                this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
+            }
+        }, false)
+    )
+
+    newInputs.forEach(
+        inputElement => inputElement.addEventListener(evt, function (e) {
+            this.previousElementSibling.innerHTML = this.value;
+            var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
+            sameElements.forEach(textElement => {
+                textElement.innerHTML = e.target.value;
+            });
+        }, false)
+    )
+
+});
+
+//DRAG ONLOAD
+$(function () {
+    $(".newInput").parent().draggable();
+});
+
+window.addEventListener("load", all);
 
     // if (window.styleMedia.type == "screen") {
 
     // }
 
-})()
