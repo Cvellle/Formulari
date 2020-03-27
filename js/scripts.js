a9c51f3a211d38b7d94296c5ea6c589d92a55528
ncvetic <cuekisa@yahoo.com>
2020-03-27 14:01:36 +0100

    a

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,26 @@
 // localStorage.removeItem("myListItems3");
 
 (function () {
+    function all() {
 
-    // PRINT WINDOW
-    const printButton = document.querySelector('#printButton');
-    printButton.onclick = function (e) {
-        window.print();
-    }
+        // PRINT WINDOW
+        const printButton = document.querySelector('#printButton');
+        printButton.onclick = function (e) {
+            window.print();
+        }
 
-    // APPEND
-    const appendInupt = function (e) {
-        const node = document.createElement("DIV");
-        const inp = document.createElement("TEXTAREA");
-        const changeFont = document.createElement("INPUT");
-        const areaName = document.createElement("INPUT");
+        // APPEND
+        const appendInupt = function (e) {
+            const node = document.createElement("DIV");
+            const inp = document.createElement("TEXTAREA");
+            const changeFont = document.createElement("INPUT");
+            const areaName = document.createElement("INPUT");
 
-        node.appendChild(inp);
-        node.insertBefore(changeFont, node.firstChild);
-        node.appendChild(areaName);
+            node.appendChild(inp);
+            node.insertBefore(changeFont, node.firstChild);
+            node.appendChild(areaName);
 
-        e.target.nextElementSibling.innerHTML += `
+            e.target.nextElementSibling.innerHTML += `
                 <div class="draggableDiv" >
                     <input type="range"/>
                     <textarea class="newInput" readonly="true"></textarea>
@@ -31,48 +32,75 @@
                 </div>
             `;
 
-        const newInputsList = document.querySelector(".myList");
-        localStorage.setItem('myListItems3', document.body.innerHTML);
+            const newInputsList = document.querySelector(".myList");
+            localStorage.setItem('myListItems3', document.body.innerHTML);
 
 
-        const newInp = document.querySelectorAll('.myList input[type="range"]');
-        newInp.forEach(inp => inp.addEventListener("mousemove", function () {
-            this.nextElementSibling.style.fontSize = this.value * 0.3 + "px";
-        }));
+            const newInp = document.querySelectorAll('.myList input[type="range"]');
+            newInp.forEach(inp => inp.addEventListener("mousemove", function () {
+                this.nextElementSibling.style.fontSize = this.value * 0.3 + "px";
+            }));
 
-        $(function () {
-            $(".newInput").parent().draggable();
-        });
+            $(function () {
+                $(".newInput").parent().draggable();
+            });
 
 
-        //  INPUT VALUES
-        "keyup change".split(" ").forEach(evt => {
-            const newInputs = document.querySelectorAll(".newInput + input");
-            const nameInputs = document.querySelectorAll(".areaName + input");
+            //  INPUT VALUES
+            "keyup change".split(" ").forEach(evt => {
+                const newInputs = document.querySelectorAll(".newInput + input");
+                const nameInputs = document.querySelectorAll(".areaName + input");
 
-            nameInputs.forEach(
-                inputElement => inputElement.addEventListener(evt, function (e) {
-                    this.previousElementSibling.innerHTML = this.value;
-                    this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
-                    //AUTO SET DATE
-                    const date = new Date();
-                    if (this.value == "date") {
-                        this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
-                    }
+                nameInputs.forEach(
+                    inputElement => inputElement.addEventListener(evt, function (e) {
+                        this.previousElementSibling.innerHTML = this.value;
+                        this.previousElementSibling.previousElementSibling.previousElementSibling.className = this.previousElementSibling.innerHTML + " newInput";
+                        //AUTO SET DATE
+                        const date = new Date();
+                        if (this.value == "date") {
+                            this.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML = `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}.`;
+                        }
+                    }, false)
+                )
+
+                newInputs.forEach(
+                    inputElement => inputElement.addEventListener(evt, function (e) {
+                        this.previousElementSibling.innerHTML = this.value;
+                        var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
+                        sameElements.forEach(textElement => {
+                            textElement.innerHTML = e.target.value;
+                        });
+                    }, false)
+                )
+
+            });
+
+            const divs = document.querySelectorAll(".draggableDiv");
+
+            divs.forEach(
+                inputElement => inputElement.addEventListener("mouseup", function (e) {
+                    localStorage.setItem('myListItems3', document.body.innerHTML);
                 }, false)
             )
 
-            newInputs.forEach(
-                inputElement => inputElement.addEventListener(evt, function (e) {
-                    this.previousElementSibling.innerHTML = this.value;
-                    var sameElements = document.querySelectorAll("." + this.nextElementSibling.innerHTML);
-                    sameElements.forEach(textElement => {
-                        textElement.innerHTML = e.target.value;
-                    });
-                }, false)
-            )
+            divs.forEach(element => {
+                element.addEventListener("mouseover", allowDel)
+            });
 
-        });
+            const deleteDiv1 = document.querySelectorAll('.myList button');
+
+            function allowDel() {
+                deleteDiv1.forEach(
+                    el => el.addEventListener("click", function (e) {
+                        this.parentElement.style.display = "none";
+                    }, false)
+                )
+            }
+
+        }
+
+        const appendInupts = document.querySelectorAll('.addInputs');
+        appendInupts.forEach(el => el.addEventListener("click", appendInupt)) = appendInupt;
 
 
         const divs = document.querySelectorAll(".draggableDiv");
@@ -87,43 +115,17 @@
             element.addEventListener("mouseover", allowDel)
         });
 
-        const deleteDiv1 = document.querySelectorAll('.myList button');
+        const deleteDiv = document.querySelectorAll('.myList button');
 
         function allowDel() {
-            deleteDiv1.forEach(
+            deleteDiv.forEach(
                 el => el.addEventListener("click", function (e) {
                     this.parentElement.style.display = "none";
                 }, false)
             )
         }
 
-    }
-
-    const appendInupts = document.querySelectorAll('.addInputs');
-    appendInupts.forEach(el => el.addEventListener("click", appendInupt)) = appendInupt;
-
-
-    const divs = document.querySelectorAll(".draggableDiv");
-
-    divs.forEach(
-        inputElement => inputElement.addEventListener("mouseup", function (e) {
-            localStorage.setItem('myListItems3', document.body.innerHTML);
-        }, false)
-    )
-
-    divs.forEach(element => {
-        element.addEventListener("mouseover", allowDel)
-    });
-
-    const deleteDiv = document.querySelectorAll('.myList button');
-
-    function allowDel() {
-        deleteDiv.forEach(
-            el => el.addEventListener("click", function (e) {
-                this.parentElement.style.display = "none";
-            }, false)
-        )
-    }
+}
 
     const drD = document.querySelectorAll(".myList>div");
     drD.forEach(
